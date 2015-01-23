d142b47555c02abb327a6d495f730869f85d095a
Choonster <Choonster.2010@gmail.com>
2015-01-24 03:35:25 +1100

    Delay logging until postInit
    -- Allows materials added through IMC (e.g. Thermal Expansion metals) to
    be logged

--- src/main/java/com/choonster/tconstructlogger/TConstructLogger.java
+++ src/main/java/com/choonster/tconstructlogger/TConstructLogger.java
@@ -4,6 +4,7 @@ import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 
 
 @Mod(modid = Constants.MODID, name = Constants.MODNAME, dependencies = "required-after:TConstruct;after:ExtraTiC")
@@ -13,7 +14,7 @@ public class TConstructLogger {
 	public static TConstructLogger instance;
 
 	@EventHandler
-	public void init(FMLInitializationEvent event) {
+	public void postInit(FMLPostInitializationEvent event) {
 		MaterialsLogger.log();
 	}
 }
